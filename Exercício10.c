a860ac334a321588f7771403d63b416df4a5eb82
GustavoWillian7 <102532016+GustavoWillian7@users.noreply.github.com>
2022-05-16 18:30:31 -0300

    Create Exercício10.c

---
+++ Exercício10.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	int vetor[5];
+	int *p = vetor;
+	int i;
+	
+	for (i=0; i<5; i++) {
+		printf("Digite o valor para a posicao %d: \n", (i+1));
+		scanf("%d", (p+i));
+	}
+	
+	printf("\n");
+	
+	for (i=0; i<5; i++) 
+		printf("Posição %d: %d\n", (i+1), *(p+i) * 2);
+	
+	return 0;
+}
